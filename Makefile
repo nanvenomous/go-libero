ae5ecf9aadc2d6da3571418d41e6f363cebf8cee
nanvenomous <mrgarelli@gmail.com>
2025-04-12 16:48:45 -0500

    switch from `Key` header to more standard `Authorization`

--- Makefile
+++
@@ -1,5 +0,0 @@
-build:
-	go build ./...
-	
-serve: build
-	./libero
--- server/libero/main.go
+++ server/libero/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,10 +15,12 @@ func main() {
 
 func checkAPIKey(w http.ResponseWriter, r *http.Request) {
 	var authd bool = true
-	apiKey := r.Header.Get("Key")
+	// apiKey := r.Header.Get("Key")
+	apiKey := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+	// fmt.Println(r.Header)
 	expectedAPIKey := os.Getenv("LIBERO_API_KEY")
 	if subtle.ConstantTimeCompare([]byte(apiKey), []byte(expectedAPIKey)) != 1 {
-		http.Error(w, "Invalid API key", http.StatusUnauthorized)
+		http.Error(w, "Invalid API key. Set the `Key` header to authorize your request.", http.StatusUnauthorized)
 		authd = false
 		return
 	}
